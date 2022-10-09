7e02031205a881dba3e4e099ad677b9e39e9bce7
Billylab <billytran75013@gmail.com>
2022-10-09 22:09:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ echo Set GOOGLE APPLICATION CREDENTIALS
 export GOOGLE_APPLICATION_CREDENTIALS=/secrets/sa-run-exercice-lincoln
 
 echo Starting Python script : feature
-python feature/feature.py
+python feature/main_feature.py
