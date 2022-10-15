09345189a3c8f04f5a5c68bce278252f5d751fec
Billylab <billytran75013@gmail.com>
2022-10-15 17:55:57 +0200

    Update invoke.go

--- invoke.go
+++ invoke.go
@@ -23,7 +23,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 		}
 
-	w.Write([]byte("translated phrase: %q\n", out.String()))
+	w.Write("translated phrase: %q\n", out.String())
+	// w.Write([]byte("translated phrase: %q\n", out.String())) MARCHE PAS
+	// w.Write([]byte("OK"))          MARCHE
+	// w.Write([]byte("%s\n", out))   MARCHE PAS
+
 	}
 
 func main() {
